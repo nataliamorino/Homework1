aa84d7f1b2d5922b40f139a5aac81032edb9ff4e
nataliamorino <morino.natalia@gmail.com>
2020-02-02 13:13:06 -0500

    Version1

---
+++ Homework1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide number of rows");
+            int NoRows = int.Parse(Console.ReadLine());
+            for (int row = NoRows; row >=1 ; row--) 
+            {
+                for (int column = row; column >= 1; column--)
+                {
+                    Console.Write(column + " ");   
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
